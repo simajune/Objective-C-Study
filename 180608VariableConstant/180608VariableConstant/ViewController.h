6e48b9c7d3ffeca9e7591f7c86270aeb2587f8b6
simajune <tejayjung@gmail.com>
2018-06-08 13:53:41 +0900

    Generate VariableConstant Project

---
+++ 180608VariableConstant/180608VariableConstant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  180608VariableConstant
+//
+//  Created by SIMA on 2018. 6. 8..
+//  Copyright © 2018년 devtejay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 180608VariableConstant/180608VariableConstant/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  180608VariableConstant
+//
+//  Created by SIMA on 2018. 6. 8..
+//  Copyright © 2018년 devtejay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSString *word1 = @"Hello";
+    NSString *const word2 = @"Goodbye";
+    
+    int a = 10;
+    const int b = 50;
+    
+    int const c = 20;
+    
+    a = 15;
+    b = 1;
+    c = 2;
+    
+    
+    word1 = @"123";
+    
+    word2 = @"456";
+    
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
