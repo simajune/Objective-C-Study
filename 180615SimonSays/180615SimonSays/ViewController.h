3b00f0f7514837600a5b07091b9f6de09b6b4264
simajune <tejayjung@gmail.com>
2018-06-18 23:32:48 +0900

    Update Simon Says Project

--- 180615SimonSays/180615SimonSays/ViewController.h
+++ 180615SimonSays/180615SimonSays/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController {
     NSTimer *timer;
+    NSTimer *simonTimer;
     
     int time;
     int score;
@@ -20,6 +21,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+@property (nonatomic, strong) UISwipeGestureRecognizer *leftSwipe;
+@property (nonatomic, strong) UISwipeGestureRecognizer *rightSwipe;
+@property (nonatomic, strong) UISwipeGestureRecognizer *upSwipe;
+@property (nonatomic, strong) UISwipeGestureRecognizer *downSwipe;
 
 - (IBAction)startBtnAction:(id)sender;
 
--- 180615SimonSays/180615SimonSays/ViewController.m
+++ 180615SimonSays/180615SimonSays/ViewController.m
@@ -16,13 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.leftSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeHandler:)];
+    self.rightSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeHandler:)];
+    self.upSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeHandler:)];
+    self.downSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeHandler:)];
+    
+    self.leftSwipe.direction = UISwipeGestureRecognizerDirectionLeft;
+    self.rightSwipe.direction = UISwipeGestureRecognizerDirectionRight;
+    self.upSwipe.direction = UISwipeGestureRecognizerDirectionUp;
+    self.downSwipe.direction = UISwipeGestureRecognizerDirectionDown;
+    
+    [self.view addGestureRecognizer:self.leftSwipe];
+    [self.view addGestureRecognizer:self.rightSwipe];
+    [self.view addGestureRecognizer:self.upSwipe];
+    [self.view addGestureRecognizer:self.downSwipe];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    self.label.layer.cornerRadius = 20;
+    self.label.clipsToBounds = YES;
+    
+    time = 20;
+    score = 0;
+    mode = 0;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) swipeHandler: (UISwipeGestureRecognizer *) sender {
+    
 }
 
 
