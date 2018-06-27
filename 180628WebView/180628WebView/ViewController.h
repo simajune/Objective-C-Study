8d8c2685ac98848d2ab40a07a2d5aac1f31cf706
simajune <tejayjung@gmail.com>
2018-06-28 02:24:42 +0900

    Generate WebView Project & Complete Part1.

---
+++ 180628WebView/180628WebView/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  180628WebView
+//
+//  Created by SIMA on 2018. 6. 28..
+//  Copyright © 2018년 devtejay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *actInd;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+
+@end
+
